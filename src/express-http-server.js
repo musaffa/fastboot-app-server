574023b2c7c5c1083b0c8d7cd98e8c65113c7003
Ahmad Musaffa <musaffa_csemm@yahoo.com>
2025-05-12 01:24:57 +0600

    Disable lastModified

--- src/express-http-server.js
+++ src/express-http-server.js
@@ -46,7 +46,9 @@ class ExpressHTTPServer {
 
     if (this.distPath) {
       app.get('/', fastbootMiddleware);
-      app.use(express.static(this.distPath));
+      app.use(express.static(this.distPath, {
+        lastModified: false
+      }));
       app.get('/assets/*', function(req, res) {
         res.sendStatus(404);
       });
